39468ee9dc467b3930358476e87216ab973da661
XingDL <xingdl2007@gmail.com>
2017-11-01 11:27:06 +0800

    15: add a better solution

--- algorithms/src/15/main.go
+++ algorithms/src/15/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
 // Find all unique triplets in the array which gives the sum of zero.
@@ -72,10 +75,50 @@ func twoSum(src int, nums []int, skip SkipList) [][]int {
 	return ret
 }
 
+// lots of bookkeeping work is eliminated if nums is sorted
+// ref: https://discuss.leetcode.com/topic/8125/concise-o-n-2-java-solution
+
+// Runtime: 1379 ms
+// Your runtime beats 100.00% of golang submissions.  -- are you kidding me?
+func threeSum2(nums []int) [][]int {
+	sort.Ints(nums)
+
+	var ret [][]int
+	for i := 0; i < len(nums)-2; i++ {
+		if i == 0 || (i > 0 && nums[i] != nums[i-1]) {
+			lo, hi, sum := i+1, len(nums)-1, -nums[i]
+			for lo < hi {
+				if nums[lo]+nums[hi] < sum {
+					lo++
+				} else if nums[lo]+nums[hi] > sum {
+					hi--
+				} else {
+					ret = append(ret, []int{nums[i], nums[lo], nums[hi]})
+
+					for lo < hi && nums[lo] == nums[lo+1] {
+						lo++
+					}
+					for lo < hi && nums[hi] == nums[hi-1] {
+						hi--
+					}
+					lo++
+					hi--
+				}
+			}
+		}
+	}
+
+	return ret
+}
+
 func main() {
 	S := []int{-1, 0, 1, 2, -1, -4}
 	fmt.Println(threeSum(S))
+	fmt.Println(threeSum2(S))
 
 	S = []int{-1, 0, 1, 2, -1, -4, 3, 1, 2, 3, 4, -12, -4, -5, -8, -1}
 	fmt.Println(threeSum(S))
+	fmt.Println(threeSum2(S))
+
+	fmt.Println(threeSum2([]int{0, 0, 0}))
 }
