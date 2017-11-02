488517e69c184ef112d031ed0664052bd79933e7
XingDL <xingdl2007@gmail.com>
2017-11-02 10:09:44 +0800

    215. Kth Largest Element in an Array, maxHeap solution practice

--- algorithms/src/215/main.go
+++ algorithms/src/215/main.go
@@ -58,6 +58,7 @@ func SetTop(heap []int, can int) {
 	}
 }
 
+// need extra space
 func MinHeap(nums []int, n int) []int {
 	// heap[0] is unused for implementation simplicity
 	heap := make([]int, n+1, n+1)
@@ -65,8 +66,9 @@ func MinHeap(nums []int, n int) []int {
 
 	// bottom-up
 	for i := n / 2; i > 0; i-- {
-		v := heap[i]
-		k := i // m is necessary
+		k := i // k is necessary
+		v := heap[k]
+
 		for done := false; !done && 2*k <= n; {
 			j := 2 * k
 			// find smaller child
@@ -85,6 +87,63 @@ func MinHeap(nums []int, n int) []int {
 		}
 	}
 	return heap
+}
+
+// Construct in-place maxHeap with bottom up method
+// same as minHeap, assume index start from 1, but use index-1 to access slice item
+func MaxHeap(nums []int) []int {
+	n := len(nums)
+	for i := n / 2; i > 0; i-- {
+		k := i
+		v := nums[k-1]
+
+		// child
+		for done := false; !done && 2*k <= n; {
+			j := 2 * k
+			if j+1 <= n && nums[j-1] < nums[j] {
+				j = j + 1
+			}
+			if v < nums[j-1] {
+				//swap
+				nums[k-1] = nums[j-1]
+				k = j
+				nums[k-1] = v
+			} else {
+				done = true
+			}
+		}
+	}
+	return nums
+}
+
+//Runtime: 12 ms, 96.67%
+func findKthLargest4(nums []int, k int) int {
+	heap := MaxHeap(nums)
+
+	// swap head with tail, reduce heap size and keep heap property
+	for i := 1; i < k; i++ {
+		size := len(heap)
+		heap[0] = heap[size-1]
+		heap = heap[0:size-1]
+		size--
+
+		j := 1
+		v := heap[j-1]
+		for done := false; !done && 2*j <= size; {
+			m := 2 * j
+			if m+1 <= size && heap[m] > heap[m-1] {
+				m = m + 1
+			}
+			if heap[j-1] < heap[m-1] {
+				heap[j-1] = heap[m-1]
+				j = m
+				heap[j-1] = v
+			} else {
+				done = true
+			}
+		}
+	}
+	return heap[0]
 }
 
 // for ref, build all nums as a heap
@@ -130,21 +189,28 @@ func Siftdown2(s []int, i int, size int) {
 }
 
 func main() {
-	//s := []int{3, 2, 1, 5, 6, 4}
+	s := []int{3, 2, 1, 5, 6, 4}
 	s2 := []int{3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6}
 
 	// test
 	//heap := MinHeap(s2, 20)
 	//fmt.Println(heap)
+	//
+	//maxheap := MaxHeap(s2)
+	//fmt.Println(maxheap)
+
 	//SetTop(heap, 7)
 	//fmt.Println(heap)
 
 	// seems works
 	//fmt.Println(findKthLargest2(s, 2))
-	fmt.Println(findKthLargest2(s2, 20))
+	//fmt.Println(findKthLargest2(s2, 20))
 
 	// verify
 	//fmt.Println(findKthLargest(s, 2))
-	fmt.Println(findKthLargest(s2, 20))
+	//fmt.Println(findKthLargest(s2, 20))
 	//fmt.Println(s2)
+
+	fmt.Println(findKthLargest4(s, 2))
+	fmt.Println(findKthLargest4(s2, 20))
 }
