78df6b7f0a26646ea50fb1206bcff9951dff6dc5
XingDL <xingdl2007@gmail.com>
2017-12-08 08:49:55 +0800

    78. Subsets (add iterative solution)

--- algorithms/src/78/main.go
+++ algorithms/src/78/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Given a set of distinct integers, nums, return all possible subsets (the power set).
 // Note: The solution set must not contain duplicate subsets.
@@ -37,9 +40,30 @@ func subsets(nums []int) [][]int {
 	return ret
 }
 
+// do it with iterative which is helpful to solve 90. Subsets II
+func subsets2(nums []int) [][]int {
+	sort.Ints(nums)
+	var res = [][]int{{}} // [] empty
+	for i := 0; i < len(nums); i++ {
+		for _, s := range res {
+			tmp := make([]int, len(s), len(s)+1)
+			copy(tmp, s)
+			tmp = append(tmp, nums[i])
+			res = append(res, tmp)
+		}
+	}
+	return res
+}
+
 func main() {
 	fmt.Println(subsets([]int{1}))
 	fmt.Println(subsets([]int{1, 2}))
 	fmt.Println(subsets([]int{1, 2, 3}))
 	fmt.Println(subsets([]int{}))
+
+	fmt.Println()
+	fmt.Println(subsets2([]int{1}))
+	fmt.Println(subsets2([]int{1, 2}))
+	fmt.Println(subsets2([]int{1, 2, 3}))
+	fmt.Println(subsets2([]int{}))
 }
