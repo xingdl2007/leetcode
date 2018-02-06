d3f948ecdadf623807d97757c92971996b28be94
XingDL <xingdl2007@gmail.com>
2018-02-06 12:10:38 +0800

    4. Median of Two Sorted Arrays

---
+++ algorithms/src/4/c++/main.cc
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
+
+  
--- algorithms/src/4/c++/medianoftwosortedarrays.cc
+++
@@ -1,8 +0,0 @@
-class Solution {
- public:
-  double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2){
-
-  }
-};
-
-  
