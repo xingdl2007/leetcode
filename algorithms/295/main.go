975513f693be23bcf35b8c1f54d5d06a898643a7
XingDL <xingdl2007@gmail.com>
2018-02-06 17:20:59 +0800

    move out of src

---
+++ algorithms/295/main.go
@@ -0,0 +1,49 @@
+package main
+
+// Median is the middle value in an ordered integer list. If the size of the list is even,
+// there is no middle value. So the median is the mean of the two middle value.
+
+// Examples:
+// [2,3,4] , the median is 3
+//
+// [2,3], the median is (2 + 3) / 2 = 2.5
+//
+// Design a data structure that supports the following two operations:
+//
+// void addNum(int num) - Insert a integer number from the data stream to the data structure.
+// double findMedian() - Return the median of all elements so far.
+//
+// For example:
+//
+// addNum(1)
+// addNum(2)
+// findMedian() -> 1.5
+// addNum(3)
+// findMedian() -> 2
+
+// TODO: heap based, skip for now
+type MedianFinder struct {
+}
+
+/** initialize your data structure here. */
+func Constructor() MedianFinder {
+
+}
+
+func (this *MedianFinder) AddNum(num int) {
+
+}
+
+func (this *MedianFinder) FindMedian() float64 {
+
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
+func main() {
+
+}
