34efa7fd3ce5f5e3e56f31dcfc64c5a843f545a8
XingDL <xingdl2007@gmail.com>
2018-02-06 12:10:15 +0800

    3. Longest Substring Without Repeating Characters

--- algorithms/src/3/c++/main.cc
+++ algorithms/src/3/c++/main.cc
@@ -3,17 +3,65 @@
 #include <map>
 using namespace std;
 
-//#include "lengthoflongestsubstring.cc"
-#include "lengthoflongestsubstring_s1.cc"
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    int pos[item_count];
+    std::fill(pos, pos + item_count, -1);
+    int i = 0, maxlen = 0;
+    for (int j = 0; j < s.size(); ++j) {
+      if (pos[s[j]] >= i) {
+        i = pos[s[j]] + 1;
+      }
+      pos[s[j]] = j;
+      maxlen = std::max(j - i + 1, maxlen);
+    }
+    return maxlen;
+  }
+private:
+  const static int item_count = 256;
+};
 
 /*
  * consider the corner case
  */
-int main(int argc, char*argv[])
-{
+int main(int argc, char *argv[]) {
   //string s("abcabcbb");
   string s("dvdf");
   //string s("yoyn");
   cout << (new Solution())->lengthOfLongestSubstring(s) << endl;
   return 0;
 }
+
+/*
+class Solution{
+public:
+  int lengthOfLongestSubstring(string s){
+    int length = 0;
+    int index = 0;
+    map<char,int> substring;
+    string::iterator iter = s.begin();
+    map<char,int>::iterator miter,duplicate;
+    for(;iter != s.end(); ++iter,++index){
+      if((duplicate=substring.find(*iter)) != substring.end()){
+        if(length < substring.size()){
+          length = substring.size();
+        }
+        for(miter=substring.begin(); miter!=substring.end();){
+          if(miter->second <= duplicate->second){
+            substring.erase(miter++);
+          }else
+            miter++;
+        }
+      }
+      substring.insert(make_pair(*iter,index));
+    }
+
+    if(length < substring.size()){
+      length = substring.size();
+    }
+
+    return length;
+  }
+};
+*/
