4672cb9983902be2148afabad2e7cedd0472916e
XingDL <xingdl2007@gmail.com>
2018-02-06 12:09:48 +0800

    2. Add Two Numbers

--- algorithms/src/2/c++/main.cc
+++ algorithms/src/2/c++/main.cc
@@ -5,26 +5,96 @@ using namespace std;
 struct ListNode {
   int val;
   ListNode *next;
-  ListNode(int x):val(x),next(NULL){}
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 /*
  * unfortunately  _s.cc has no speedup.
  */
-//#include "addtwonumbers.cc"
-#include "addtwonumbers_s.cc"
+/*
+ * Definition for singly-linked list.
+ *
+ *
+ * struct ListNode {
+ *   int val;
+ *   ListNode *next;
+ *   ListNode(int x):val(x),next(NULL){}
+ * };
+ */
 
-void printlist(ListNode *l){
-  if(l==NULL) return;
-  while(l){
+class Solution {
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *rst, *pre, *cur;
+    int carry = 0, sum;
+
+    if (l1 == NULL)
+      return l2;
+    if (l2 == NULL)
+      return l1;
+
+    // alloc a useless head node, just to simplify program
+    rst = new ListNode(0);
+    pre = cur = rst;
+
+    while (l1 && l2) {
+      sum = l1->val + l2->val + carry;
+      carry = sum/10;
+      sum %= 10;
+      cur = new ListNode(sum);
+      pre->next = cur;
+      pre = cur;
+
+      l1 = l1->next;
+      l2 = l2->next;
+    }
+
+    while (l1) {
+      sum = l1->val + carry;
+      carry = sum/10;
+      sum %= 10;
+      l1->val = sum;
+      cur = l1;
+      pre->next = cur;
+      pre = cur;
+      l1 = l1->next;
+    }
+
+    while (l2) {
+      sum = l2->val + carry;
+      carry = sum/10;
+      sum %= 10;
+      l2->val = sum;
+      cur = l2;
+      pre->next = cur;
+      pre = cur;
+      l2 = l2->next;
+    }
+
+    if (carry) {
+      cur = new ListNode(carry);
+      pre->next = cur;
+    }
+
+    /*del the useless head node*/
+    cur = rst;
+    rst = rst->next;
+    delete cur;
+    return rst;
+  }
+};
+
+void printlist(ListNode *l) {
+  if (l == NULL)
+    return;
+  while (l) {
     cout << l->val << " -> ";
     l = l->next;
   }
-  cout<<endl;
+  cout << endl;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   ListNode *l1 = NULL, *l2 = NULL;
 
   l1 = new ListNode(2);
@@ -37,7 +107,66 @@ int main(int argc, char* argv[])
   l2->next->next = new ListNode(4);
   printlist(l2);
 
-  printlist((new Solution())->addTwoNumbers(l2,l1));
-  
+  printlist((new Solution())->addTwoNumbers(l2, l1));
+
   return 0;
 }
+
+
+/*
+ * class Solution{
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+    ListNode *rst,*pre,*cur;
+    int carry = 0,sum;
+
+    if(l1==NULL) return l2;
+    if(l2==NULL) return l1;
+
+    // alloc a useless head node, just to simplify program
+    rst = new ListNode(0);
+    pre = cur = rst;
+
+    while(l1 && l2){
+      sum = l1->val + l2->val + carry;
+      carry = sum / 10;
+      sum %= 10;
+      cur = new ListNode(sum);
+      pre->next = cur;
+      pre = cur;
+
+      l1 = l1->next;
+      l2 = l2->next;
+    }
+
+    while(l1){
+      sum = l1->val + carry;
+      carry = sum / 10;
+      sum %= 10;
+      cur = new ListNode(sum);
+      pre->next = cur;
+      pre = cur;
+      l1 = l1->next;
+    }
+
+    while(l2){
+      sum = l2->val + carry;
+      carry = sum / 10;
+      sum %= 10;
+      cur = new ListNode(sum);
+      pre->next = cur;
+      pre = cur;
+      l2 = l2->next;
+    }
+
+    if(carry){
+      cur = new ListNode(carry);
+      pre->next = cur;
+    }
+    cur = rst;
+    rst = rst->next;
+    delete cur;
+    return rst;
+  }
+};
+*/
